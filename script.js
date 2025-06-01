40d173d3cd2f19e52aa07c88f1a2cee59fc8653e
been-03 <nkb0306@naver.com>
2025-06-01 19:43:17 +0900

    단순명제 기능 추가

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let newCompound = document.querySelector('#new-compound');
 let addCompoundBtn = document.querySelector('#add-compound-btn');
 let compoundList = document.querySelector('#compound-list');
 
+let newPrimary = document.querySelector('#new-primary');
+let addPrimaryBtn = document.querySelector('#add-primary-btn');
+let primaryList = document.querySelector('#primary-list');4
+
 addCompoundBtn.addEventListener('click', function() {
     // Create a new compound item
     let li = document.createElement('li');
@@ -18,4 +22,21 @@ addCompoundBtn.addEventListener('click', function() {
     compoundList.appendChild(li);
     newCompound.value = ''; // Clear the input field after adding
     newCompound.focus(); // Focus back on the input field 
+});
+
+addPrimaryBtn.addEventListener('click', function() {
+    // Create a new primary item
+    let li = document.createElement('li');
+    li.textContent = newPrimary.value;
+
+    let btn = document.createElement('button');
+    btn.textContent = '[X]';
+    btn.className = 'remove-btn';
+    btn.addEventListener('click', function() {
+        primaryList.removeChild(li);
+    })
+    li.appendChild(btn);
+    primaryList.appendChild(li);
+    newPrimary.value = '';
+    newPrimary.focus();
 });
